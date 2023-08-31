37201af7769fd1c973ceb9c2b78d9b700f9d24b3
张嘉凯 <killicestone@126.com>
2023-08-31 09:24:57 +0800

    feat:该分支新增页面

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,23 +1,5 @@
 <template>
   <view class="content">
-    <view class="title">
-      环卫巡查端
-    </view>
-    <view class="formControl">
-      <view class="forInputItem">
-        <view class="formTitle">
-          账号
-        </view>
-        <uni-easyinput class="uni-mt-5" trim="all" v-model="accountVal" placeholder="请输入账号"></uni-easyinput>
-      </view>
-
-      <view class="forInputItem">
-        <view class="formTitle">
-          密码
-        </view>
-        <uni-easyinput class="uni-mt-5" trim="all" v-model="passwordVal" placeholder="请输入密码"></uni-easyinput>
-      </view>
-    </view>
   </view>
 </template>
 
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,12 +1,33 @@
 <template>
   <view class="login">
 
-    login
+    <view class="title">
+      环卫巡查端
+    </view>
+    <view class="formControl">
+      <view class="forInputItem">
+        <view class="formTitle">
+          账号
+        </view>
+        <uni-easyinput class="uni-mt-5" trim="all" v-model="accountVal" placeholder="请输入账号"></uni-easyinput>
+      </view>
+
+      <view class="forInputItem">
+        <view class="formTitle">
+          密码
+        </view>
+        <uni-easyinput class="uni-mt-5" trim="all" v-model="passwordVal" placeholder="请输入密码"></uni-easyinput>
+      </view>
+    </view>
   </view>
 </template>
 
 <script setup lang="ts">
 
+import {ref} from "vue";
+
+let accountVal = ref('')
+let passwordVal = ref('')
 </script>
 
 <style scoped lang="less">

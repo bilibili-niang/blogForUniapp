9fef42b0bd025ffa27c286885b8060293a0b47f1
张嘉凯 <killicestone@126.com>
2023-09-04 00:18:25 +0800

    feat:uniHelper

--- src/components/common/iceSwiper.vue
+++ src/components/common/iceSwiper.vue
@@ -1,10 +1,36 @@
 <template>
   <div class="iceSwiper">
-    swiper
+    <swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="true" interval="2000"
+            duration="500"
+            @change="durationChange"
+    >
+      <swiper-item>
+        <view class="swiper-item uni-bg-red">A</view>
+      </swiper-item>
+      <swiper-item>
+        <view class="swiper-item uni-bg-green">B</view>
+      </swiper-item>
+      <swiper-item>
+        <view class="swiper-item uni-bg-blue">C</view>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from "vue";
+
+const indicatorDots = ref(true)
+// 使用uniHelper提供的类型来声明
+const durationChange: UniHelper.SwiperOnChange = (event) => {
+  console.log('change')
+  console.log(event)
+  // 获取index
+  console.log(event.detail?.current)
+  // 非空断言,表示detail不可能为空
+  console.log(event.detail!.current)
+
+}
 
 </script>
 
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,46 +2,14 @@
   <customNavBar></customNavBar>
   <iceSwiper></iceSwiper>
   <view class="content">
-    <text>当前登录用户信息:</text>
-    <text>
-      {{ memberStore.profile }}
-    </text>
-    <button @tap="memberStore.setProfile({
-    nickname:'测试用户名',
-    token:'1234567890'
-    })">set token
-    </button>
-    <button @tap="memberStore.clearProfile()">清空</button>
-    <button @tap="requestTest">请求</button>
-    <button @tap="requestTestFail">失败请求</button>
+
   </view>
 </template>
 
 <script setup lang="ts">
-import {useMemberStore} from "@/stores";
-import {ref} from 'vue'
-import api from "@/utils/api";
 import CustomNavBar from "@/pages/index/components/customNavBar.vue";
 import IceSwiper from "@/components/common/iceSwiper.vue";
 
-const memberStore = useMemberStore()
-
-const title = ref('Hello')
-/**
- * 获取首页数据
- */
-const requestTest = async () => {
-  const res = await api.home()
-  console.log(res)
-}
-const requestTestFail = async () => {
-  const res = await api.homeFail()
-  if (res.success) {
-    console.log(res)
-  } else {
-    console.log('fail')
-  }
-}
 </script>
 
 <style scoped lang="less">
